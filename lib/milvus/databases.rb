1c8b73ba205dd589ab9800e0ef473e0eeef5e3bd
Michael Bock <mb@rapidionline.com>
2025-01-05 18:26:35 +0100

    adding support for databases

---
+++ lib/milvus/databases.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module Milvus
+  class Databases < Base
+    PATH = "databases"
+
+    # This operation alters a database.
+    #
+    # @param db_name [String] The name of the database to alter.
+    # @param properties [String] The new properties of the database.
+    # @return [Hash] Server response
+    def alter(db_name:)
+      response = client.connection.post("#{PATH}/alter") do |req|
+        req.body = {
+          dbName: db_name,
+          properties:
+        }
+      end
+      response.body
+    end
+
+    # This operation creates a new database.
+    #
+    # @param db_name [String] The name of the database to be created.
+    # @param properties [String] The properties of the new database.
+    # @return [Hash] Server response
+    def create(db_name:, properties:)
+      response = client.connection.post("#{PATH}/create") do |req|
+        req.body = {
+          dbName: db_name,
+          properties:
+        }
+      end
+      response.body.empty? ? true : response.body
+    end
+
+    # This operation describes a database.
+    #
+    # @param db_name [String] The name of the database to describe.
+    # @return [Hash] Server response
+    def describe(db_name:)
+      response = client.connection.post("#{PATH}/describe") do |req|
+        req.body = {
+          dbName: db_name
+        }
+      end
+      response.body
+    end
+
+    # This operation lists all databases.
+    #
+    # @return [Hash] Server response
+    def list
+      response = client.connection.post("#{PATH}/list") do |req|
+        req.body = {}
+      end
+      response.body
+    end
+
+    # This operation drops the database and all collections within the database.
+    #
+    # @param db_name [String] The name of the database to drop.
+    # @return [Hash] Server response
+    def drop(db_name:)
+      response = client.connection.post("#{PATH}/drop") do |req|
+        req.body = {
+          dbName: db_name
+        }
+      end
+      response.body.empty? ? true : response.body
+    end
+
+  end
+end
